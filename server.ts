fd360aca4093099cfa49090f75b9241a05d908b9
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-07 14:08:57 +0545

    setup express server

--- server.ts
+++ server.ts
@@ -1,3 +1,14 @@
-console.log("Welcome to code baby");
+import app from "./src/app"
+
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Listening on port: ${port}`);
+    })
+};
+
+
+startServer();
 
 
