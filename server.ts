642e711d87a3d67fae42ef2372028e943ef69317
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-09 08:07:17 +0545

    connected Database

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import { config } from "./src/config/config";
 import app from "./src/app"
 
-const startServer = () => {
+import connectDB from "./src/config/db";
+
+const startServer = async () => {
+
+    await connectDB();
     const port = config.port || 3000;
 
     app.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-app.get('/', (req,res,next) => {
+app.get('/', (req,res) => {
 res.json({message:"welcome to ebook apis"})
 })
 
