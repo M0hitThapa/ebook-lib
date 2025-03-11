0458f1c4abd38928b1b9c5dc407ab996755ad2ca
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-11 23:03:54 +0545

    setup cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,12 +1,20 @@
 
 import { Request, Response,NextFunction } from "express"
+import cloudinary from "../config/cloudinary";
 
 const createBook = async (
     req: Request,
     res: Response,
      next: NextFunction) => {
-        
 
+console.log('files', req.files);
+// const coverImageMimeType = req.files.coverImage
+
+// const uploadResult = await cloudinary.uploader.upload(filePath, {
+//         filename_override: filename,
+//         folder:'book-covers',
+//         format:coverImageMimeType,
+// })
 
         res.json({});
 } ;
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import createBook from "./bookController";
 import multer from "multer";
-import path from "node:path";
+import path from "path";
+
+
 
 const bookRouter = express.Router();
 
 const upload = multer({
-    dest:path.resolve(__dirname,'../../public/data/uploads'),
-    limits:{fileSize:3e7}  //30mb
+    dest: path.resolve(__dirname,'../../public/data/uploads'),
+    limits:{fileSize:3e7},  //30mb
 })
 
 bookRouter.post('/',upload.fields([
-    {name:"coverImage", maxCount: 1},
-    {name:"file", maxCount: 1},
+    {name:'coverImage', maxCount: 1},
+    {name:'file', maxCount: 1}
 
-]), createBook)
+]), createBook);
 
 
 
