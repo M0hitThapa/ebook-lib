611ec8fcdadcc7be3c51c67e45cec55a42b5b8f4
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-21 00:08:13 +0545

    build book card component

--- ebook-client-app/src/components/BookCard.tsx
+++ ebook-client-app/src/components/BookCard.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 
 const BookCard = ({book}: {book: Book}) => {
   return (
-    <div className='flex gap-5 border p-5 shadow-md'>
-        <Image src={book.coverImage} alt={book.title} width={0} height={0} sizes='100vw' style={{width:'auto', height:'12rem'}}/>
+    <div className='flex gap-5  border px-5 py-2 shadow-md rounded-lg'>
+        <Image className='rounded-lg' src={book.coverImage} alt={book.title} width={0} height={0} sizes='100vw' style={{width:'auto', height:'12rem'}}/>
         <div>
             <h2 className='line-clamp-2 text-xl font-bold text-teal-600 text-balance'>{book.title}</h2>
             <p className='font-bold text-teal-900 mt-1'>{book.author.name}</p>
-            <Link href={`/book/${book._id}`} className=' py-2 px-2 rounded border border-teal-500 mt-2 inline-block'>Read More</Link>
+            <Link href={`/book/${book._id}`} className=' py-2 px-2 rounded border border-teal-500 mt-2 inline-block text-teal-500 font-medium text-sm hover:border-teal-100 hover:bg-teal-50 transition'>Read More</Link>
 
         </div>
     </div>
